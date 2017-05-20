ce2483ab471fc37c85ccb6f1a30fe782e1fa51e8
mkimaev <maxim.kimaev@gmail.com>
2017-05-20 21:33:01 +0300

    using class Parallel

---
+++ My_Matix_Filler/Filler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace My_Matix_Filler
+{
+    class Filler
+    {
+        int curPosRow = 0;
+        int curPosCol = 0;
+        /// <summary>
+        /// прорисовка слева направо
+        /// </summary>
+        public void WriteSymbols()
+        {
+            if (curPosCol == 118)
+            {
+                curPosCol = 0;
+            }
+            Random randomNumber = new Random();
+            int randColor = randomNumber.Next(15); //получаем случайное число меньше 15
+            Console.ForegroundColor = (ConsoleColor)randColor; //красим символы
+            for (int j = randomNumber.Next(1, 25); j < 30; j++) //пускаем 1 колонку случайной длины
+            {
+                //char sym = (char)randomNumber.Next(30, 79); // получаем случайные символы
+                Console.SetCursorPosition(curPosCol, curPosRow++); //устанавливаем позицию курсора и сдвиг на 1 вниз
+                //Console.Write(sym);
+                Console.Write("Parallel");
+                Thread.Sleep(2);
+            }
+            curPosCol++; //передвигаем курсор на 1 колонку правее
+            curPosRow = 0; //обнуляем позицию строки
+            Console.Clear();
+
+        }
+        /// <summary>
+        /// прорисовка справа налево
+        /// </summary>
+        public void WriteSymbolsReverse()
+        {
+            if (curPosCol == 0)
+            {
+                curPosCol = 118;
+            }
+            Random randomNumReverse = new Random();
+            int randColor = randomNumReverse.Next(15);
+            Console.ForegroundColor = (ConsoleColor)randColor;
+            for (int j = randomNumReverse.Next(1, 25); j < 30; j++)
+            {
+                //char sym = (char)randomNumReverse.Next(30, 79);
+                Console.SetCursorPosition(curPosCol, curPosRow++);
+                //Console.Write(sym);
+                Console.WriteLine("Parallel");
+                Thread.Sleep(2);
+            }
+            curPosCol--; //передвигаем курсор на 1 колонку левее
+            curPosRow = 0;
+        }
+    }
+}
--- My_Matix_Filler/Program.cs
+++ My_Matix_Filler/Program.cs
@@ -3,33 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace My_Matix_Filler
 {
     class Program
     {
-        static int curPosRow = 0;
-        static int curPosRow2 = 0;
-        static int curPosCol = 0;
-        static int curPosCol2 = 118;
-        static object block = new object();
         static void Main(string[] args)
         {
+            Filler fill = new Filler();
+            Filler fill2 = new Filler();
             try
-            { 
-                
-            Thread[] threads = new Thread[60];
-            Thread[] threads2 = new Thread[60];
-            if (threads.Length != threads2.Length) { throw new OwnException("Длины массивов отличаются, что привело к ошибке!"); }
-            for (int i = 0; i < threads.Length; i++)
             {
-                threads[i] = new Thread(new ThreadStart(WriteSymbols)); //инициализация потока c применением делtгата ThreadStart
-                threads2[i] = new Thread(() => { WriteSymbolsReverse(); }); //инициализация потока c применением лямбды
-                threads[i].Start(); //старт
-                threads2[i].Start();
-                //threads[i].Join();
-                threads2[i].Join(); //ожидаем пока поток завершиться
-            }
+                while (true)
+                {
+                    //Parallel.Invoke(fill.WriteSymbols, fill2.WriteSymbolsReverse);
+                    Parallel.Invoke(new Action(fill.WriteSymbols), new Action (fill2.WriteSymbolsReverse));
+                    /*Parallel.Invoke(() => fill.WriteSymbols(),
+                                      () => fill2.WriteSymbolsReverse());*/
+                }
+                //if (threads.Length != threads2.Length) { throw new OwnException("Длины массивов отличаются, что привело к ошибке!"); }
             }
             catch (OwnException ex)
             {
@@ -38,49 +31,6 @@ namespace My_Matix_Filler
                 Console.WriteLine("Пример рабочей версии посмотрите тут {0}", ex.HelpLink);
             }
             Console.ReadLine();
-        }
-        /// <summary>
-        /// прорисовка слева направо
-        /// </summary>
-        static void WriteSymbols()
-        {
-            //используем приёмы для синхронизацию к разделяемому ресурсу
-            lock (block) // Monitor.Enter(block)
-            {
-                Random randomNumber = new Random();
-                int randColor = randomNumber.Next(15); //получаем случайное число меньше 15
-                Console.ForegroundColor = (ConsoleColor)randColor; //красим символы
-                for (int j = randomNumber.Next(1, 25); j < 30; j++) //пускаем 1 колонку случайной длины
-                {
-                    char sym = (char)randomNumber.Next(30, 79); // получаем случайные символы
-                    Console.SetCursorPosition(curPosCol, curPosRow++); //устанавливаем позицию курсора и сдвиг на 1 вниз
-                    Console.Write(sym);
-                    Thread.Sleep(2);
-                }
-                curPosCol++; //передвигаем курсор на 1 колонку правее
-                curPosRow = 0; //обнуляем позицию строки
-            } // Monitor.Exit(block)
-        }
-        /// <summary>
-        /// прорисовка справа налево
-        /// </summary>
-        static void WriteSymbolsReverse()
-        {
-            //lock (block)
-            {
-                Random randomNumReverse = new Random();
-                int randColor = randomNumReverse.Next(15);
-                Console.ForegroundColor = (ConsoleColor)randColor;
-                for (int j = randomNumReverse.Next(1, 25); j < 30; j++)
-                {
-                    char sym = (char)randomNumReverse.Next(30, 79);
-                    Console.SetCursorPosition(curPosCol2, curPosRow2++);
-                    Console.Write(sym);
-                    Thread.Sleep(2);
-                }
-                curPosCol2--; //передвигаем курсор на 1 колонку левее
-                curPosRow2 = 0;
-            }
         }
     }
 }
